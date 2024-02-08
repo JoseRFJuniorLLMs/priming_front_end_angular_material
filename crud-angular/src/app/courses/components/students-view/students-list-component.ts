870f96eccf66050714b73ef5e523d53935eb67d7
JoseRFJuniorMedibands <144800596+JoseRFJuniorMedibands@users.noreply.github.com>
2024-02-08 20:06:52 +0000

    update student list

--- crud-angular/src/app/courses/components/students-view/students-list-component.ts
+++ crud-angular/src/app/courses/components/students-view/students-list-component.ts
@@ -1,10 +1,16 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+import { CategoryPipe } from '../../../shared/pipes/category.pipe';
 import { StudentCollection } from '../../model/student-collection';
 
 @Component({
   selector: 'app-students-list',
   templateUrl: './students-list.component.html',
-  styleUrls: ['./students-list.component.scss']
+  styleUrls: ['./students-list.component.scss'],
+  standalone: true,
+  imports: [MatTableModule, MatIconModule, MatButtonModule, CategoryPipe]
 })
 export class StudentsListComponent {
   @Input() students: StudentCollection[] = [];
